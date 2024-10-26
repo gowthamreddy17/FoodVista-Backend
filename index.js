b4c6d3b93571be9dc92c1b8618902f6321aa864c
gowthamreddy17 <saigowthamreddy1497@gmail.com>
2024-10-26 21:42:23 +0530

    updated index.js for port and home route

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import productRoutes from "./routes/productRoutes.js";
 import path from "path";
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 dotEnv.config();
 // app.use(express.json());
@@ -19,9 +19,9 @@ mongoose
   .then(() => console.log("mongoDb connected"))
   .catch((err) => console.log(err));
 
-// app.use("/home", (req,res)=>{
-//     res.send("<h1>Hello")
-// });
+app.use("/home", (req,res)=>{
+    res.send("<h1>Hello from Vista</h1>")
+});
 
 //middlewares
 app.use("/vendor", vendorRoutes);
